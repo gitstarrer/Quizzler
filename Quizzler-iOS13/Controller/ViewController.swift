1fdd1e8fd8a6a855757cd15e9d5dfe3ae8025a3a
gitstarrer <67234999+gitstarrer@users.noreply.github.com>
2020-07-27 23:37:09 +0530

    initial commit

---
+++ Quizzler-iOS13/Controller/ViewController.swift
@@ -0,0 +1,67 @@
+//
+//  ViewController.swift
+//  Quizzler-iOS13
+//
+//  Created by Angela Yu on 12/07/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var questionLabel: UILabel!
+  
+    @IBOutlet weak var firstButton: UIButton!
+    
+    @IBOutlet weak var secondButton: UIButton!
+    
+    @IBOutlet weak var thirdButton: UIButton!
+    
+    
+    var score : String?
+    var quizBrain = QuizBrain()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+         scoreLabel.text = "Score: 0"
+        // Do any additional setup after loading the view.
+        updateUI()
+    }
+
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        
+        let userAnswer = sender.currentTitle!
+        let userGotItRight = quizBrain.checkAnswer(userAnswer)
+        
+        if userGotItRight{
+            sender.backgroundColor = UIColor.green
+        }else {
+            sender.backgroundColor = UIColor.red
+        }
+    
+    quizBrain.nextQuestion()
+        
+        Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(updateUI), userInfo: nil, repeats: false)
+        
+       
+        
+    }
+    
+    @objc func updateUI(){
+        questionLabel.text = quizBrain.getQuestionText()
+        firstButton.backgroundColor = UIColor.clear
+        secondButton.backgroundColor = UIColor.clear
+        thirdButton.backgroundColor = UIColor.clear
+        progressBar.progress = quizBrain.getProgress()
+        scoreLabel.text = "Score:  \(quizBrain.getScore())"
+        
+        firstButton.setTitle(quizBrain.quiz[quizBrain.questionNumber].answer[0], for: .normal)
+        secondButton.setTitle(quizBrain.quiz[quizBrain.questionNumber].answer[1], for: .normal)
+        thirdButton.setTitle(quizBrain.quiz[quizBrain.questionNumber].answer[2], for: .normal)
+        
+        
+    }
+}
+
---
+++ README.md
@@ -0,0 +1,9 @@
+
+#  Quizzler
+
+## About the App
+
+This is a trivia quiz app. If you have another field of expertise such as law or medicine, you’ll be glad to know that multiple choice question apps are one of the most popular types of educational apps on the App Store! 
+
+* This app conforms to the holy grail of mobile design patterns: the Model View Controller (MVC) pattern.
+
